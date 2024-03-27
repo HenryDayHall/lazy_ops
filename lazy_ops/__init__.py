fac16cd6ec55b764a94f2f495c901a528129c06f
Henry Day-Hall <henry.day-hall@cern.ch>
2024-03-27 13:44:04 +0100

    Added behavior required for use as a submodules

--- lazy_ops/__init__.py
+++ lazy_ops/__init__.py
@@ -1,4 +1,8 @@
-from lazy_ops.lazy_loading import DatasetView
-from lazy_ops.lazy_loading import lazy_transpose
+try:
+    from lazy_ops.lazy_loading import DatasetView
+    from lazy_ops.lazy_loading import lazy_transpose
+except ImportError:
+    from .lazy_loading import DatasetView
+    from .lazy_loading import lazy_transpose
 
 from .version import version as __version__
